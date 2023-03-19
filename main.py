6ef462964942878d62d75102ae5e632fd5fc12f3
Vlads Sokolovs <118102879+5oK0L@users.noreply.github.com>
2023-03-20 00:59:57 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,50 +1,48 @@
-def build_heap(data):
-    swaps = []
-    n = len(data)
-    
-    for i in range(n // 2 - 1, -1, -1):
-        j = i
-        while True:
-            k =2*j + 1
-            if k >= n:
-                break
-            if k +1 < n and data[k + 1] < data[k]:
-                k += 1
-            if data[j] > data[k]:
-                swaps.append((j, k))
-                data[j], data[k] = data[k], data[j]
-                j = k
-            else:
-                break  
-  
-    return swaps
+def parallel_processing(n, m, data):
+    output = []
+    g = []
+    for i in range(n):
+        g.append(0)
+    d = 0
+    t = 0
+    while d < m:
+        for i in range(n):
+            if g[i] == t:
+                output.append((i, t))
+                g[i] = g[i] + data[d]
+                d = d + 1
+        t = t + 1
+    # TODO: write the function for simulating parallel tasks, 
+    # create the output pairs
 
+    return output
 
 def main():
+    # TODO: create input from keyboard
+    # input consists of two lines
+    # first line - n and m
+    # n - thread count 
+    # m - job count
+    n = 0
+    m = 0
+    r = input().split()
+    n = int(r[0])
+    m = int(r[1])
+
+    # second line - data 
+    # data - contains m integers t(i) - the times in seconds it takes any thread to process i-th job
+    data = []
+    r = input().split()
+    for i in range(m):
+        data.append(int(r[i]))
+
+    # TODO: create the function
+    result = parallel_processing(n,m,data)
     
-    read_input = input()
-    
-    if read_input.startswith('I'):
-        n = int(input())
-        data = list(map(int, input().split()))
-        assert len(data) == n
-        swaps = build_heap(data)
-        print(len(swaps))
-        for i, j in swaps:
-            print(i, j)
-            
-    elif read_input.startswith('F'):
-        file = input().strip()
-        with open(f'tests/{file}', 'r') as f:
-            n = int(f.readline().strip())
-            data = list(map(int, f.readline().split()))
-        assert len(data) == n
-        swaps = build_heap(data)
-        print(len(swaps))
-        
-    else:
-        print('Invalid character')
-     
+    # TODO: print out the results, each pair in it's own line
+    for i in range(m):
+        print(result[i][0], result[i][1])
+
 
 
 if _name_ == "_main_":
